ba567949c4a9edbcc127a9df4c66bf35e7d3b8c3
whydk <49267125+whydk@users.noreply.github.com>
2019-04-04 10:15:43 +0300

    Add files via upload

--- Scripts/Hex.cs
+++ Scripts/Hex.cs
@@ -1,1 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Hex
+{
+    public readonly int Q; //Column
+    public readonly int R; //Row
+    public readonly int S;
+    static readonly float WIDTH_MULT = Mathf.Sqrt(3) / 2;
+    public Hex(int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.S = (-q + r);
+    }
+    public Vector3 Position()
+    {
+        float radius = 1f;
+        float height = radius * 2;
+        float width = WIDTH_MULT * height;
+        float horizontal = width;
+        float verical = height * 3 / 4;
+        return new Vector3(
+            horizontal * (Q + R / 2f),
+            verical * R,
+            0);
+    }
+}
